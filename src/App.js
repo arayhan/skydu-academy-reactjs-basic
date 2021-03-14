8878c0d0244085edd7b5b0cdc28f90ee33562247
Rayhan <rayhan@badr.co.id>
2021-03-14 15:08:24 +0700

    2.1 - JSX Super Power

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const renderButton = () => {
+		return (
+			<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Learn React
+			</a>
+		);
+	};
+
+	const title = "Skydu Academy";
+	const pokemonList = [
+		{
+			name: "Bulbasaur",
+			color: "Green",
+		},
+		{
+			name: "Charizard",
+			color: "Red",
+		},
+	];
+
+	const inputAttr = {
+		type: "email",
+		placeholder: "masukkan email",
+		required: true,
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img
+					style={{ width: "80rem" }}
+					src="./assets/images/logo.svg"
+					className="App-logo"
+					alt="logo"
+				/>
+				<p>Hello World, {title}</p>
+				<input {...inputAttr} />
+				<ul>
+					{pokemonList.map((pokemon) => (
+						<li>{pokemon.color}</li>
+					))}
+				</ul>
+				{renderButton()}
+			</header>
+		</div>
+	);
 }
 
 export default App;
