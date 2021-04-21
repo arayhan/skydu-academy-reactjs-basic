bac736ea3f33f85b84522b341d8607eb4f4c11e6
Rayhan <rayhan@badr.co.id>
2021-04-21 21:24:40 +0700

    Video 5.2 - berpindah halaman menggunakan react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import { Component } from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Detail from "./pages/Detail";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<nav>
-					<ul>
-						<li>
-							<a href="/">Home</a>
-						</li>
-						<li>
-							<a href="/about">About</a>
-						</li>
-					</ul>
-				</nav>
-				<Home />
-			</div>
+			<BrowserRouter>
+				<div className="App">
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/about">About</Link>
+							</li>
+						</ul>
+					</nav>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/about" component={About} />
+						<Route path="/detail" component={Detail} />
+						<Route component={NotFound} />
+					</Switch>
+				</div>
+			</BrowserRouter>
 		);
 	}
 }
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Card.css";
 
 const Card = (props) => {
@@ -6,6 +7,7 @@ const Card = (props) => {
 		<div className={`card ${props.isDarkMode ? "darkMode" : ""}`}>
 			<h1>{props.title}</h1>
 			<p onCopy={() => props.handleCopyDeskripsi()}>{props.deskripsi}</p>
+			<Link to="/detail">Read More</Link>
 			<button onClick={() => props.handleLike()}>Like</button> : {props.like}
 		</div>
 	);
