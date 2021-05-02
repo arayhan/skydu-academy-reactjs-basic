4342bc627f02c9806a07704f236bd9648ec91e9d
Rayhan <rayhan@badr.co.id>
2021-05-03 00:36:05 +0700

    6.2 Get Details

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
 
 class Detail extends Component {
+	state = {
+		params: this.props.match.params.pokemon,
+		details: null,
+	};
+
+	componentDidMount() {
+		this.fetchPokemonDetail();
+	}
+
+	fetchPokemonDetail = () => {
+		fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.params}`)
+			.then((data) => data.json())
+			.then((json) => this.setState({ details: json }));
+	};
+
 	render() {
-		const params = this.props.match.params;
-		const pokemon = params.pokemon;
-		return <div>Detail Pokemon : {pokemon}</div>;
+		const { details, params } = this.state;
+		return (
+			<div>
+				<div>Detail Pokemon : {params}</div>
+				{!details && <div>Loading Details...</div>}
+				{details && (
+					<div>
+						<h1>Sprites</h1>
+						<img src={details.sprites.back_default} alt="" />
+						<img src={details.sprites.back_female} alt="" />
+						<img src={details.sprites.back_shiny} alt="" />
+						<img src={details.sprites.back_shiny_female} alt="" />
+						<img src={details.sprites.front_default} alt="" />
+					</div>
+				)}
+			</div>
+		);
 	}
 }
 
